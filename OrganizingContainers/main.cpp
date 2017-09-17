f5e52d582f59bce5b70c285d908771bb5c2148e5
glm3531 <glm3531@gmail.com>
2017-09-17 15:45:28 -0600

    Solved.

--- OrganizingContainers/main.cpp
+++ OrganizingContainers/main.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
-
-void printMatrix(vector<vector<int> > matrix, int n) {
-
-    for (int i=0;i<n;i++) {
-        for (int j=0;j<n;j++) {
-            cout<<matrix[i][j]<<" ";
+string solve(vector<vector<long long> > M, int n)
+{
+    vector<long long> sum_col(n,0),sum_row(n,0);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                sum_col[i]+=M[i][j];
+            }
         }
-        cout<<"\n";
-    }
-}
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                sum_row[i]+=M[j][i];
+            }
+        }
+        sort(sum_col.begin(),sum_col.end());
+        sort(sum_row.begin(),sum_row.end());
 
+        if(sum_col==sum_row)
+            return "Possible";
+        else return "Impossible";
+}
 
 int main()
 {
@@ -23,16 +37,16 @@ int main()
     while (t-- > 0) {
         int n;
         cin >> n;
-        vector<vector<int> > matrix(2, vector<int>(2, 0));
-        for (int i=0;i<n;i++){
-            for (int j=0;j<n;j++) {
-                int num; cin>>num;
+        vector<vector<long long> > matrix(n, vector<long long>(n, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int num;
+                cin >> num;
                 matrix[i][j] = num;
             }
         }
-        printMatrix(matrix, n);
+        cout << solve(matrix, n) << endl;
     }
-
 
     return 0;
 }
